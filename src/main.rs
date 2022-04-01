d63e5e096acd746048a39b2a56f4a8cfb7de4d1d
David Wang <millimillenary@gmail.com>
2022-04-02 10:22:48 +1100

    Print out code pegs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+type CodePegs = [i8; 4];
+
+fn format_pegs(x: &CodePegs) -> String {
+    format!("{} {} {} {}", x[0], x[1], x[2], x[3])
+}
+
+fn generate_puzzle() -> CodePegs {
+    [1, 2, 3, 4]
+}
+
 fn main() {
-    println!("Hello, world!");
+    let solution = generate_puzzle();
+    println!("solution: {}", format_pegs(&solution));
 }
